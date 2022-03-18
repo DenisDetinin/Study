aeecda7e6df8c99044ba683ed5cad40a5c3ee69c
Пользователь <DenisDetinin>
2022-03-19 02:47:22 +0500

    first commit

---
+++ D1/script.js
@@ -0,0 +1,30 @@
+alert("Привет мир")
+let Cl1 = prompt('Введите 1 число ');
+let a = prompt('введите операцию');
+let cl2 = prompt('введите число 2');
+let b = confirm('Действительно ли вы хотите выполнить данную операцию');
+if (a !== "" && !isNaN(Cl1) && !isNaN(cl2)) {
+    if (b) {
+
+        if (a == "+") {
+            alert(+Cl1 + +cl2);
+        }
+
+        if (a == "-") {
+            alert(+Cl1 - +cl2);
+        }
+
+        if (a == "*");
+        {
+            alert(+Cl1 * +cl2);
+        }
+
+        if (a == "/") {
+            alert(+Cl1 / cl2);
+        }
+    } else {
+        alert('нет')
+    }
+} else {
+    alert('Ошибка')
+}
